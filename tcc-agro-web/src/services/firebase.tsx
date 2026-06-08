2cddceee45fbd395ec1245182bbca53f38fe102d
osouzinha <souzerado@gmail.com>
2026-06-08 18:07:13 -0300

    fix: chaves banco

--- tcc-agro-web/src/services/firebase.tsx
+++ tcc-agro-web/src/services/firebase.tsx
@@ -2,8 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCt...", // Suas chaves
-  databaseURL: "https://tcc-agro-st-default-rtdb.firebaseio.com"
+  apiKey: "AIzaSyCt...",
+  databaseURL: "https://tcc-agro-st-default-rtdb.firebaseio.com",
 };
 
 const app = initializeApp(firebaseConfig);
